b15de8bec7dcbbf1923176486ed06fde55bc9324
DanielaThais <daniela.thais64@gmail.com>
2026-05-12 21:39:48 -0300

    Trabalhando com String Builder

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,14 @@
-import java.time.Duration;
-import java.time.OffsetDateTime;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		var stringStart = OffsetDateTime.now();
-		String stringConcat = "";
-
-		for (int i = 0; i < 400_000; i++) {
-			stringConcat += i; // essa concatenação é muito custosa, pois a cada concatenação é criado um novo
-								// objeto String, o que consome muita memória e tempo de processamento, não
-								// sendo a forma mais indicada.
-		}
-		var stringEnd = OffsetDateTime.now();
-		System.out.printf("String concatenada: %s \n", getInterval(stringStart, stringEnd));
-
-		// essas são as formas recomendadas para concatenar strings em Java, pois o
-		// StringBuilder é uma classe mutável que permite modificar o conteúdo sem criar
-		// novos objetos, o que é mais eficiente em termos de memória e desempenho.Vale
-		// ressaltar que semrpre é bom fazer uma análise de mercado
-
-		var BuilderStart = OffsetDateTime.now();
-		var stringBuilder = new StringBuilder(); // singlethreads
-		for (int i = 0; i < 400_000; i++) {
-			stringBuilder.append(i);
-		}
-		var BuilderEnd = OffsetDateTime.now();
-		System.out.printf("StringBuilder (singlethreads): %s \n",
-				getInterval(BuilderStart, BuilderEnd));
-
-		var BufferStart = OffsetDateTime.now();
-		var stringBuffer = new StringBuffer(); // multithreads
-		for (int i = 0; i < 400_000; i++) {
-			stringBuffer.append(i);
-		}
-		var BufferEnd = OffsetDateTime.now();
-
-		System.out.printf("StringBuffer (multithreads): %s \n", getInterval(BufferStart, BufferEnd));
-
+		var builder = new StringBuilder("1234567890");
+		System.out.println("<======== APRESENTANDO DADOS ========>" + "\n\nDados iniciais: " + builder);
+		System.out.println("Dados após builder.delete (0, 3): " + builder.delete(0, 3)); 
+		System.out.println("Dados após builder.deleteCharAt (0): " +builder.deleteCharAt(0)); 
+		System.out.println("Dados com builder.reverse: " + builder.reverse());
+		System.out.println("Reinserindo dados: " + builder.append("43210").reverse());
 	}
-
-	private static long getInterval(OffsetDateTime BufferStart, OffsetDateTime BufferEnd) {
-		return Duration.between(BufferStart, BufferEnd).toMillis();
-	}
+	
 }
