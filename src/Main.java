0d9a7d51994fba1314d63193ddf89822ac00d889
DanielaThais <daniela.thais64@gmail.com>
2026-05-14 16:03:37 -0300

    Entendo como double, Double e Float são interpretados no Java

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,56 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		var builder = new StringBuilder("1234567890");
-		System.out.println("<======== APRESENTANDO DADOS ========>" + "\n\nDados iniciais: " + builder);
-		System.out.println("Dados após builder.delete (0, 3): " + builder.delete(0, 3)); 
-		System.out.println("Dados após builder.deleteCharAt (0): " +builder.deleteCharAt(0)); 
-		System.out.println("Dados com builder.reverse: " + builder.reverse());
-		System.out.println("Reinserindo dados: " + builder.append("43210").reverse());
+		// trabalhando com o tipo Double
+		var value_double1 = 0.1;
+		var value_double2 = 0.1;
+
+		System.out.println(value_double1 + value_double2);
+		System.out.println("\n<============= TIPO DOUBLE =============>");
+		System.out.println(2.00 - 1.1);
+		System.out.println(2.00 - 1.2);
+		System.out.println(2.00 - 1.3);
+		System.out.println(2.00 - 1.4);
+		System.out.println(2.00 - 1.5);
+		System.out.println(2.00 - 1.6);
+		System.out.println(2.00 - 1.7);
+		System.out.println(2.00 - 1.8);
+		System.out.println(2.00 - 1.9);
+		System.out.println(2.00 - 2);
+
+		// Trabalhando com o objeto Float
+		Float value1 = 0.1f;
+		Float value2 = 0.2f;
+
+		System.out.println(value1 + value2);
+		System.out.println("\n<============= OBJETO FLOAT =============>");
+		System.out.println(new Float(2.00f) - new Float(1.1f));
+		System.out.println(new Float(2.00f) - new Float(1.2f));
+		System.out.println(new Float(2.00f) - new Float(1.3f));
+		System.out.println(new Float(2.00f) - new Float(1.4f));
+		System.out.println(new Float(2.00f) - new Float(1.5f));
+		System.out.println(new Float(2.00f) - new Float(1.6f));
+		System.out.println(new Float(2.00f) - new Float(1.7f));
+		System.out.println(new Float(2.00f) - new Float(1.8f));
+		System.out.println(new Float(2.00f) - new Float(1.9f));
+		System.out.println(new Float(2.00f) - new Float(2f));
+
+		// Trabalhando com o objeto Double
+		Double value3 = 0.1;
+		Double value4 = 0.2;
+
+		System.out.println(value3 + value4);
+		System.out.println("\n<============= OBJETO DOUBLE =============>");
+		System.out.println(new Double(2.00) - new Double(1.1));
+		System.out.println(new Double(2.00) - new Double(1.2));
+		System.out.println(new Double(2.00) - new Double(1.3));
+		System.out.println(new Double(2.00) - new Double(1.4));
+		System.out.println(new Double(2.00) - new Double(1.5));
+		System.out.println(new Double(2.00) - new Double(1.6));
+		System.out.println(new Double(2.00) - new Double(1.7));
+		System.out.println(new Double(2.00) - new Double(1.8));
+		System.out.println(new Double(2.00) - new Double(1.9));
+		System.out.println(new Double(2.00) - new Double(2));
 	}
-	
+
 }
